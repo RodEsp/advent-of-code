80f4c51f01f10b5f16bf845cdcb59d4a1cf1bbf8
rodesp <1084688+RodEsp@users.noreply.github.com>
2023-12-02 12:06:24 -0500

    chore: clean up day 2 part 1 a bit

--- 2023/puzzles/two/part1.js
+++ 2023/puzzles/two/part1.js
@@ -6,25 +6,17 @@ const rl = rline('../../data/puzzle_2.txt');
 let getGameInfo = (line) => {
 	return {
 		gameNumber: Number(line.match(/Game\s([0-9]*)/)[1]),
-		blues: [...line.matchAll(/([0-9]*)\sblue/g)].map((match) => Number(match[1])),
-		reds: [...line.matchAll(/([0-9]*)\sred/g)].map((match) => Number(match[1])),
-		greens: [...line.matchAll(/([0-9]*)\sgreen/g)].map((match) => Number(match[1]))
+		reds: Math.max(...[...line.matchAll(/([0-9]*)\sred/g)].map((match) => Number(match[1]))),
+		greens: Math.max(...[...line.matchAll(/([0-9]*)\sgreen/g)].map((match) => Number(match[1]))),
+		blues: Math.max(...[...line.matchAll(/([0-9]*)\sblue/g)].map((match) => Number(match[1]))),
 	};
 };
 
 let sum = 0;
 rl.on('line', async (line) => {
-	const { gameNumber, blues, reds, greens } = getGameInfo(line);
+	const { gameNumber, reds, greens, blues } = getGameInfo(line);
 
-	for (const num of blues) {
-		if (num > 14) return;
-	}
-	for (const num of reds) {
-		if (num > 12) return;
-	}
-	for (const num of greens) {
-		if (num > 13) return;
-	}
+	if (reds > 12 || greens > 13 || blues > 14) return;
 	sum += gameNumber;
 });
 
