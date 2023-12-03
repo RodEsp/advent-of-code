ee01c16947da82ece8be557908e514ee49e8a087
rodesp <1084688+RodEsp@users.noreply.github.com>
2023-12-03 13:26:25 -0500

    feat: day 3 part 1

---
+++ 2023/puzzles/three/part1.js
@@ -0,0 +1,71 @@
+import { readfile } from "../../utils/getInput.js";
+
+let sum = 0;
+
+/**
+ * @param {string[][]} engineDiagram 
+ */
+const getSum = async (engineDiagram) => {
+	// r and c correspond to the indeces for the rows and columns of the engineDiagram
+	let r = 0, c = 0;
+
+	while (r < engineDiagram.length) {
+		const { number, end } = findNextNumber(c, engineDiagram[r]);
+		if (number) {
+			c = end;
+			if (isPartNumber(engineDiagram, number, r, c - number.length)) {
+				sum += Number(number);
+			}
+		} else {
+			c = 0;
+			r++;
+		}
+	}
+};
+
+const findNextNumber = (c, row) => {
+	let number = '';
+
+	while (c < row.length) {
+		if (!isNaN(row[c])) {
+			number += row[c];
+		} else if (number !== '') {
+			break;
+		}
+		c++;
+	}
+
+	return {
+		number,
+		end: c
+	};
+};
+
+const isPartNumber = (engineDiagram, number, r, c) => {
+	if (r > 0) {
+		// Check row above for symbols
+		for (let i = c - 1; i < c + number.length + 1; i++) {
+			if (isSymbol(engineDiagram[r - 1][i])) return true;
+		}
+	}
+
+	// Check sides for symbols
+	if (isSymbol(engineDiagram[r][c - 1])) return true;
+	if (isSymbol(engineDiagram[r][c + number.length])) return true;
+
+	if (r < engineDiagram.length - 1) {
+		// Check row below for symbols
+		for (let i = c - 1; i < c + number.length + 1; i++) {
+			if (isSymbol(engineDiagram[r + 1][i])) return true;
+		}
+	}
+
+	return false;
+};
+
+const isSymbol = (char) => char !== '.' && char !== undefined && isNaN(char);
+
+// This gets the input file and splits it into a 2D array of characters
+getSum((await readfile('../../data/puzzle_3.txt')).split('\n').map(line => line.split('')));
+
+console.log(sum);
--- 2023/utils/getInput.js
+++ 2023/utils/getInput.js
@@ -1,6 +1,12 @@
 import { createReadStream } from 'node:fs';
+import { readFile } from 'node:fs/promises';
 import { createInterface } from 'node:readline/promises';
 
+/**
+ * Returns an event emitter that reads data in a file line by line (see https://nodejs.org/docs/latest-v20.x/api/readline.html#readline)
+ * @param {string} filepath 
+ * @returns 
+ */
 export const readline = (filepath) => {
 	const fileStream = createReadStream(filepath);
 	return createInterface({
@@ -8,3 +14,10 @@ export const readline = (filepath) => {
 		crlfDelay: Infinity
 	});
 };
+
+/**
+ * Returns a promise that resolves to the contents of a file as text
+ * @param {string} filepath 
+ * @returns 
+ */
+export const readfile = async (filepath) => await readFile(filepath, { encoding: 'utf-8' }); 
