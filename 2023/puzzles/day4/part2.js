5a1f2aaa35cbd5eb1b1babfd07b19d8514723697
rodesp <1084688+RodEsp@users.noreply.github.com>
2023-12-05 14:43:49 -0500

    fix: clean up day 4 part 2 code a bit

--- 2023/puzzles/day4/part2.js
+++ 2023/puzzles/day4/part2.js
@@ -2,42 +2,13 @@ import { readfile } from "../../utils/getInput.js";
 
 const cards = new Map();
 
-const addWins = (card) => {
-	for (let i = 0; i < card.instances; i++) {
-		for (let w = 1; w <= card.wins; w++) {
-			let c = cards.get(card.number + w);
-
-			if (!c) {
-				c = {
-					number: card.number + w,
-					instances: 1,
-					wins: undefined,
-				};
-				cards.set(card.number + w, c);
-			} else {
-				c.instances += 1;
-			}
-		}
-	}
-};
-
 const getSum = (input) => {
 	let sum = 0;
 
 	for (let i = 0; i < input.length; i++) {
-		const [cardNumber, winners, guesses] = input[i].match(/Card\s+(\d*):\s([\d\s]+)\s\|\s([\d\s]+)/).slice(1, 4).map((str) => str.match(/([0-9]+)/g)).map(a => a.length === 1 ? Number(a[0]) : a.map(num => Number(num)));
-		let card = cards.get(cardNumber);
+		const [id, winners, guesses] = input[i].match(/Card\s+(\d*):\s([\d\s]+)\s\|\s([\d\s]+)/).slice(1, 4).map((str) => str.match(/([0-9]+)/g)).map(a => a.length === 1 ? Number(a[0]) : a.map(num => Number(num)));
 
-		if (!card) {
-			card = {
-				number: cardNumber,
-				instances: 1,
-				wins: undefined,
-			};
-			cards.set(cardNumber, card);
-		} else {
-			card.instances += 1;
-		}
+		const card = addCard(id);
 
 		let matches = 0;
 		for (let i = 0; i < guesses.length; i++) {
@@ -52,6 +23,31 @@ const getSum = (input) => {
 	sum = Array.from(cards.values()).reduce((sum, card) => sum += card.instances, sum);
 
 	console.log(sum);
+};
+
+const addCard = (id) => {
+	let card = cards.get(id);
+
+	if (!card) {
+		card = {
+			number: id,
+			instances: 1,
+			wins: undefined,
+		};
+		cards.set(id, card);
+	} else {
+		card.instances += 1;
+	}
+
+	return card;
+};
+
+const addWins = (card) => {
+	for (let i = 0; i < card.instances; i++) {
+		for (let w = 1; w <= card.wins; w++) {
+			addCard(card.number + w);
+		}
+	}
 };
 
 // This gets the input file and splits it into lines
