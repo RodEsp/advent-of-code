84acab05f0e0c0fe2fcc37f45cef440ac5eb0e02
rodesp <1084688+RodEsp@users.noreply.github.com>
2023-12-05 10:16:38 -0500

    feat: Day 4 part 1

---
+++ 2023/puzzles/four/part1.js
@@ -0,0 +1,20 @@
+import { once } from "node:events";
+import { readline } from "../../utils/getInput.js";
+
+let sum = 0;
+const getSum = (line) => {
+	// Forms the input line into an array of winning numbers and guessed numbers
+	const [winners, guesses] = line.match(/[0-9\s]+\|[0-9\s]+/)[0].split('|').map((str) => str.match(/([0-9]{1,2})/g));
+
+	let matches = 0;
+	for (let i = 0; i < guesses.length; i++) {
+		if (winners.includes(guesses[i])) matches++;
+	}
+
+	sum += matches > 0 ? Math.pow(2, matches - 1) : 0;
+};
+
+// This reads each line of the input file asynchronously and waits until all of them have been read.
+await once(readline('../../data/puzzle_4.txt').on('line', getSum), 'close');
+
+console.log(sum);
