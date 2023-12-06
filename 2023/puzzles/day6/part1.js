bf42d44d5dc3098118fa7a181f1b38d298f5db76
rodesp <1084688+RodEsp@users.noreply.github.com>
2023-12-06 12:11:56 -0500

    feat: day 6 part 1

---
+++ 2023/puzzles/day6/part1.js
@@ -0,0 +1,35 @@
+import { EOL } from "os";
+import { readfile } from "../../utils/getInput.js";
+
+const parseInput = (input) => {
+	let [times, distances] = input.split(EOL).map(line => line.match(/\d+/g).map(n => Number(n)));
+
+	const races = [];
+	for (let i = 0; i < times.length; i++) {
+		races.push({
+			time: times[i],
+			distance: distances[i]
+		});
+	}
+
+	return races;
+};
+
+const races = parseInput(await readfile('../../data/6.txt'));
+
+const quadraticEquation = ({ totaltime: b, distance: c }) => {
+	b = b * -1;
+	const root_part = Math.sqrt(b * b - 4 * c);
+
+	var upper_root = (-b + root_part) / 2;
+	var lower_root = (-b - root_part) / 2;
+
+	return [upper_root, lower_root];
+};
+
+const product = races.reduce((product, race) => {
+	let [upper, lower] = quadraticEquation({ totaltime: race.time, distance: race.distance + 1 });
+	return product *= Math.floor(upper) - Math.ceil(lower) + 1;
+}, 1);
+
+console.log(product);
