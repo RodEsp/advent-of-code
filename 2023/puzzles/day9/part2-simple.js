1d266f8422e6b998c0fa2f4ab02df249e46a7b32
rodesp <1084688+RodEsp@users.noreply.github.com>
2023-12-09 09:33:55 -0500

    feat: add an even simpler solution for day 9 part 2

---
+++ 2023/puzzles/day9/part2-simple.js
@@ -0,0 +1,29 @@
+import { once } from "node:events";
+import { readline } from "../../utils/getInput.js";
+
+let sum = 0;
+const getSum = async (line) => {
+	const sequences = [line.split(' ').map((n) => Number(n)).reverse()];
+
+	let lastSeq = sequences[sequences.length - 1];
+	let nextSeq;
+	while (lastSeq.some((num) => num !== 0)) {
+		nextSeq = [];
+		for (let i = 1; i < lastSeq.length; i++) {
+			nextSeq.push(lastSeq[i] - lastSeq[i - 1]);
+		}
+		sequences.push(nextSeq);
+		lastSeq = nextSeq;
+	}
+
+	for (let i = sequences.length - 2; i >= 0; i--) {
+		sequences[i].push(sequences[i + 1][sequences[i + 1].length - 1] + sequences[i][sequences[i].length - 1]);
+	}
+
+	sum += sequences[0][sequences[0].length - 1];
+};
+
+// This reads each line of the input file asynchronously and waits until all of them have been read.
+await once(readline('../../data/9.txt').on('line', getSum), 'close');
+
+console.log(sum);
