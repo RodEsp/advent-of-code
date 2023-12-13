cdc66446055c7f5a0fdab0699e42e17e7ecc5db0
rodesp <1084688+RodEsp@users.noreply.github.com>
2023-12-13 11:28:02 -0500

    feat: day 13 part 1

---
+++ 2023/puzzles/day13/part1.js
@@ -0,0 +1,56 @@
+import { EOL, type } from 'os';
+
+import { transpose } from '../../utils/functions.js';
+import { readfile } from '../../utils/getInput.js';
+
+let patterns = [...(await readfile('../../data/13.txt')).split(`${EOL}${EOL}`).map((pattern) => pattern.split(EOL).map(row => row.split('')))];
+let remainingPatterns = [];
+let sum = 0;
+
+function checkIfReflection (r1, r2, pattern) {
+	if (r1 < 0 || r2 >= pattern.length) {
+		return true;
+	}
+	return pattern[r1].toString() === pattern[r2].toString() && checkIfReflection(r1 - 1, r2 + 1, pattern);
+}
+
+function checkRowsForReflection (pattern) {
+	let foundReflection = false;
+	for (let r = 0; r < pattern.length - 1; r++) {
+		const row = pattern[r];
+		const nextRow = pattern[r + 1];
+
+		if (row.toString() == nextRow.toString()) {
+			foundReflection = checkIfReflection(r, r + 1, pattern);
+		}
+
+		if (foundReflection) {
+			return r;
+		}
+	}
+
+	if (!foundReflection) {
+		remainingPatterns.push(pattern);
+	}
+
+	return null;
+}
+
+// Check rows for reflections
+patterns.forEach(pattern => {
+	const reflectionRow = checkRowsForReflection(pattern);
+	if (reflectionRow !== null) {
+		sum += 100 * (reflectionRow + 1);
+	}
+});
+
+// Check columns for reflections
+remainingPatterns.forEach(pattern => {
+	pattern = transpose(pattern);
+	const reflectionRow = checkRowsForReflection(pattern);
+	if (reflectionRow !== null) {
+		sum += reflectionRow + 1;
+	}
+});
+
+console.log(sum);
--- 2023/utils/functions.js
+++ 2023/utils/functions.js
@@ -1,1 +1,20 @@
 export const isNumber = (number) => !isNaN(number);
+
+export const memoize = (fn) => {
+	const cache = new Map();
+
+	return (...args) => {
+		const key = JSON.stringify(args);
+
+		if (cache.has(key)) return cache.get(key);
+
+		const result = fn(...args);
+		cache.set(key, result);
+		return result;
+	};
+};
+
+// https://en.wikipedia.org/wiki/Transpose
+export const transpose = (matrix) => {
+	return matrix[0].map((_, colIndex) => matrix.map(row => row[colIndex]));
+};
